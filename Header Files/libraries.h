636bbeac889251c8462f9b16db80e940ea0e836f
KaranBengali <karanbengali4@gmail.com>
2017-11-21 12:18:50 -0700

    Includes opencv and standard IO libraries

---
+++ Header Files/libraries.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+/* OpenCV Libraries */
+#include<opencv2\core\core.hpp>
+#include<opencv2\highgui\highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+#include<opencv2\ml\ml.hpp>
+
+#define SEE_IMAGES 1
